f1bcb1ff1717d278c87fd6eef3f34788ed913b8d
Evan <131621972+evanL7@users.noreply.github.com>
2023-10-28 21:31:25 -0700

    updated

--- a4_sort_implementations.h
+++ a4_sort_implementations.h
@@ -159,12 +159,77 @@ Sort_stats shell_sort(vector<T> &v)
     return Sort_stats{"shell sort", v.size(), num_comps, elapsed_cpu_time_sec};
 }
 
-/*
+
+// Helper function for merge sort
+template <typename T>
+void merge(vector<T> &left, vector<T> &right, vector<T> &v, ulong &num_comps)
+{
+    int L_sz = left.size();
+    int R_sz = right.size();
+    int i = 0, j = 0, k = 0;
+
+    while (j < L_sz && k < R_sz)
+    {
+        num_comps++;
+        if (left[j] < right[k])
+        {
+            v[i] = left[j];
+            j++;
+        }
+        else
+        {
+            v[i] = right[k];
+            k++;
+        }
+        i++;
+    }
+    while (j < L_sz)
+    {
+        v[i] = left[j];
+        i++; j++;
+    }
+    while (k < R_sz)
+    {
+        v[i] = right[k];
+        i++; k++;
+    }
+}
+
+
+// Helper function for merge sort
+template <typename T>
+void merge_sort_void(vector<T> &v, ulong &num_comps)
+{
+    if (v.size() <= 1) { return; }
+
+    int mid = v.size() / 2;
+    vector<T> left;
+    vector<T> right;
+
+    // Copies data to left vector and right vector
+    for (int i = 0; i < mid; i++)
+    {
+        left.push_back(v[i]);
+    }
+    for (int i = 0; i < v.size()-mid; i++)
+    {
+        right.push_back(v[i+mid]);
+    }
+
+    merge_sort_void(left, num_comps);
+    merge_sort_void(right, num_comps);
+    merge(left, right, v, num_comps);
+}
+
+
+// Adapted from https://www.geeksforgeeks.org/merge-sort/
 template <typename T>
 Sort_stats merge_sort(vector<T> &v)
 {
     ulong num_comps = 0;
     clock_t start = clock();
+
+    merge_sort_void(v, num_comps);
 
     clock_t end = clock();
     double elapsed_cpu_time_sec = double(end - start) / CLOCKS_PER_SEC;
@@ -172,7 +237,7 @@ Sort_stats merge_sort(vector<T> &v)
     return Sort_stats{"merge sort", v.size(), num_comps, elapsed_cpu_time_sec};
 }
 
-
+/*
 template <typename T>
 Sort_stats quick_sort(vector<T> &v)
 {
@@ -197,18 +262,93 @@ Sort_stats iquick_sort(vector<T> &v)
     
     return Sort_stats{"iquick sort", v.size(), num_comps, elapsed_cpu_time_sec};
 }
+*/
+
+// Helper class for priority queue sort
+// Adapted from week 7 lecture notes
+template <typename T>
+class PriorityQueue_heap
+{
+    vector<T> v;
+    ulong num_comps = 0; // Stores the number of comparisons within the class
+
+public:
+
+    // Returns the number of elements in the priority queue
+    int size() const { return v.size(); }
 
 
+    bool empty() const { return v.size() == 0; }
+
+
+    ulong comparisons() const { return num_comps; }
+
+
+    void insert(const T &x)
+    {
+        v.push_back(x);
+        int i = size() - 1;
+
+        while (i > 0 && v[i] < v[(i-1) / 2])
+        {
+            num_comps++;
+            swap(v[i], v[(i-1) / 2]);
+            i = (i-1) / 2;
+        }
+    }
+
+
+    const T &min() const { return v[0]; }
+
+
+    void remove_min()
+    {
+        if (empty()) { return; }
+
+        v[0] = v.back();
+        v.pop_back();
+        int i = 0, j;
+
+        while (2 * i + 1 < size())
+        {
+            num_comps++;
+            j = 2 * i + 1;
+            if (j + 1 < size() && v[j + 1] < v[j])
+            {
+                j++;
+            }
+            if (v[i] <= v[j])
+            {
+                break;
+            }
+            swap(v[i], v[j]);
+            i = j;
+        }
+    }
+}; // class PriorityQueue_heap
+
+
+// Adapted from week 7 lecture notes
 template <typename T>
 Sort_stats priority_queue_sort(vector<T> &v)
 {
-    ulong num_comps = 0;
     clock_t start = clock();
+
+    PriorityQueue_heap<T> pq;
+
+    while (v.size() > 0) // Loop to put all elements into the priority queue
+    {
+        pq.insert(v.back());
+        v.pop_back();
+    }
+    while (pq.size() > 0)
+    {
+        v.push_back(pq.min()); // Push min element back to the vector
+        pq.remove_min(); // Remove min from the priority queue
+    }
 
     clock_t end = clock();
     double elapsed_cpu_time_sec = double(end - start) / CLOCKS_PER_SEC;
     
-    return Sort_stats{"priority queue sort", v.size(), num_comps, elapsed_cpu_time_sec};
+    return Sort_stats{"priority queue sort", v.size(), pq.comparisons(), elapsed_cpu_time_sec};
 }
-
-*/
