c65490eca353bcfe99a7be8a10eafe1c6f61651e
Evan <131621972+evanL7@users.noreply.github.com>
2023-11-12 21:06:07 -0800

    updated

--- a5_Wordlist.h
+++ a5_Wordlist.h
@@ -55,10 +55,9 @@ class Wordlist : public Wordlist_base
     {
         string word;
         int count;
+        int height; // Keeps track of the node's height to add words
         Node *left;
         Node *right;
-
-        int height; // Keeps track of the node's height
     };
 
     Node *root = nullptr;
@@ -70,6 +69,8 @@ class Wordlist : public Wordlist_base
     //
     // No variables other than root are permitted!
     //
+
+public:
 
     // Default constructor
     Wordlist() {}
@@ -93,27 +94,24 @@ class Wordlist : public Wordlist_base
 
 
     // Destructor for Wordlist
-    ~Wordlist()
-    {
-        freeTree(root);
-        root = nullptr; // Prevent dangling pointers
-    }
+    ~Wordlist() { freeTree(root); }
 
 
     // Helper function for the destructor that deallocates memory
     void freeTree(Node *n)
     {
-        if (n != nullptr) // Uses recursion to delete the tree
+        if (n != nullptr) // Uses recursion
         {
             freeTree(n->left);
             freeTree(n->right);
-            free(n);
+            delete n;
+            n = nullptr; // Prevent dangling pointers
         }
     }
 
 
     // Helper function that searches through the tree using recursion and returns a node
-    Node *search(Node *n, const string &w)
+    Node *search(Node *n, const string &w) const
     {
         // Word does not exist in the tree or word exists
         if (n == nullptr || n->word == w) { return n; }
@@ -138,7 +136,7 @@ class Wordlist : public Wordlist_base
 
 
     // Helper function for num_different_words method
-    int num_nodes(Node *n, int &count)
+    int num_nodes(Node *n, int &count) const
     {
         if (n != nullptr)
         {
@@ -158,7 +156,7 @@ class Wordlist : public Wordlist_base
 
 
     // Helper function for total_words method
-    int tot_words(Node *n, int &count)
+    int tot_words(Node *n, int &count) const
     {
         if (n != nullptr)
         {
@@ -178,7 +176,7 @@ class Wordlist : public Wordlist_base
 
     // Helper function for is_sorted
     // Adapted from ChatGPT
-    bool check_sorted(Node* n, const std::string& minVal = "", const std::string& maxVal = "")
+    bool check_sorted(Node* n, const std::string& minVal = "", const std::string& maxVal = "")const
     {
         if (n == nullptr) {
             return true; // An empty tree is considered sorted
@@ -197,24 +195,30 @@ class Wordlist : public Wordlist_base
 
     string most_frequent() const
     {
-        Node *max = find_max(root);
-        return max->word + max->count;
+        Node *max_ptr = root;
+        Node *max = find_max(root, max_ptr);
+        return "\"" + max->word + " " + std::to_string(max->count) + "\"";
     }
 
 
     // Helper function for most_frequent method
-    Node *find_max(Node *n, Node *&max_node)
+    // Adapted from ChatGPT
+    Node *find_max(Node *n, Node *&max_node) const
     {
-        if (n == nullptr) { return; }
-        
-        find_max(n->left, max_node);
+        if (n == nullptr) { return nullptr; }
 
-        if (max_node->count < n->count)
-        {
-            max_node = n;
-        }
-        
-        find_max(n->right, max_node);
+        Node *left_max = find_max(n->left, max_node);
+        Node *right_max = find_max(n->right, max_node);
+
+        // Compare left subtree max
+        if (left_max != nullptr && left_max->count > n->count) { max_node = left_max; }
+
+        // Compare right subtree max
+        if (right_max != nullptr && right_max->count > n->count) { max_node = right_max; }
+
+        // Compare current node with the overall max
+        if (max_node->count < n->count) { max_node = n; }
+
         return max_node;
     }
 
@@ -222,17 +226,17 @@ class Wordlist : public Wordlist_base
     int num_singletons() const
     {
         int total = 0;
-        return count_singletons(root, total);
+        total = count_singletons(root, total);
+        return total;
     }
 
 
-    int count_singletons(Node *n, int &count)
+    int count_singletons(Node *n, int &count) const
     {
         if (n != nullptr)
         {
-            num_nodes(n->left, count);
-            num_nodes(n->right, count);
-
+            count_singletons(n->left, count);
+            count_singletons(n->right, count);
             if (n->count == 1) { count++; }
         }
         return count;
@@ -242,144 +246,152 @@ class Wordlist : public Wordlist_base
     // Adapted from https://www.geeksforgeeks.org/insertion-in-an-avl-tree/
     void add_word(const string &w)
     {
-        Node *new_root = insert(w);
+        Node *new_root = insert(root, w);
         root = new_root;
     }
 
 
-int height(Node *N)  
-{  
-    if (N == NULL)  
-        return 0;  
-    return N->height;  
-}  
-  
-// A utility function to get maximum 
-// of two integers  
-int max(int a, int b)  
-{  
-    return (a > b)? a : b;  
-}  
-  
-/* Helper function that allocates a  
-   new node with the given key and  
-   NULL left and right pointers. */
-Node* create_node(string key)
-{  
-    Node *n = new Node(); 
-    n->key = key;  
-    n->left = NULL;  
-    n->right = NULL;  
-    n->height = 1; // new node is initially 
-                      // added at leaf  
-    return(n);  
-}  
-  
-// A utility function to right 
-// rotate subtree rooted with y  
-// See the diagram given above.  
-Node *rightRotate(Node *y)  
-{  
-    Node *x = y->left;  
-    Node *T2 = x->right;  
-  
-    // Perform rotation  
-    x->right = y;  
-    y->left = T2;  
-  
-    // Update heights  
-    y->height = max(height(y->left), 
-                    height(y->right)) + 1;  
-    x->height = max(height(x->left), 
-                    height(x->right)) + 1;  
-  
-    // Return new root  
-    return x;  
-}  
-  
-// A utility function to left  
-// rotate subtree rooted with x  
-// See the diagram given above.  
-Node *leftRotate(Node *x)  
-{  
-    Node *y = x->right;  
-    Node *T2 = y->left;  
-  
-    // Perform rotation  
-    y->left = x;  
-    x->right = T2;  
-  
-    // Update heights  
-    x->height = max(height(x->left),     
-                    height(x->right)) + 1;  
-    y->height = max(height(y->left),  
-                    height(y->right)) + 1;  
-  
-    // Return new root  
-    return y;  
-}  
-  
-// Get Balance factor of node N  
-int getBalance(Node *N)  
-{  
-    if (N == NULL)  
-        return 0;  
-    return height(N->left) - height(N->right);  
-}  
-  
-// Recursive function to insert a key 
-// in the subtree rooted with node and 
-// returns the new root of the subtree.  
-Node* insert(Node* node, string key)  
-{  
-    /* 1. Perform the normal BST insertion */
-    if (node == NULL)  
-        return(create_node(key));  
-  
-    if (key < node->key)  
-        node->left = insert(node->left, key);  
-    else if (key > node->key)  
-        node->right = insert(node->right, key);  
-    else // Equal keys are not allowed in BST  
-        return node;  
-  
-    /* 2. Update height of this ancestor node */
-    node->height = 1 + max(height(node->left),  
-                        height(node->right));  
-  
-    /* 3. Get the balance factor of this ancestor  
-        node to check whether this node became  
-        unbalanced */
-    int balance = getBalance(node);  
-  
-    // If this node becomes unbalanced, then  
-    // there are 4 cases  
-  
-    // Left Left Case  
-    if (balance > 1 && key < node->left->key)  
-        return rightRotate(node);  
-  
-    // Right Right Case  
-    if (balance < -1 && key > node->right->key)  
-        return leftRotate(node);  
-  
-    // Left Right Case  
-    if (balance > 1 && key > node->left->key)  
+    int height(Node *n)  
     {  
-        node->left = leftRotate(node->left);  
-        return rightRotate(node);  
+        if (n == nullptr) { return 0; }
+        return n->height;  
+    }  
+    
+    // A utility function to get maximum 
+    // of two integers  
+    int max(int a, int b)  
+    {  
+        return (a > b)? a : b;  
     }  
   
-    // Right Left Case  
-    if (balance < -1 && key < node->right->key)  
-    {  
-        node->right = rightRotate(node->right);  
-        return leftRotate(node);  
-    }  
-  
-    /* return the (unchanged) node pointer */
-    return node;  
-}  
+    // Allocates memory for a node in the tree
+    // count = 1 because inserting a word adds to the count
+    // height = 1 because new node is initially added at leaf
+    Node* create_node(string key) { return new Node{key, 1, 1, nullptr, nullptr}; }
 
+
+    // A utility function to right 
+    // rotate subtree rooted with y  
+    Node *rightRotate(Node *y)  
+    {  
+        Node *x = y->left;  
+        Node *T2 = x->right;  
+    
+        // Perform rotation  
+        x->right = y;  
+        y->left = T2;  
+    
+        // Update heights  
+        y->height = max(height(y->left), 
+                        height(y->right)) + 1;  
+        x->height = max(height(x->left), 
+                        height(x->right)) + 1;  
+    
+        // Return new root  
+        return x;  
+    }  
+    
+    // A utility function to left  
+    // rotate subtree rooted with x  
+    Node *leftRotate(Node *x)  
+    {  
+        Node *y = x->right;  
+        Node *T2 = y->left;  
+    
+        // Perform rotation  
+        y->left = x;  
+        x->right = T2;  
+    
+        // Update heights  
+        x->height = max(height(x->left),     
+                        height(x->right)) + 1;  
+        y->height = max(height(y->left),  
+                        height(y->right)) + 1;  
+    
+        // Return new root  
+        return y;  
+    }  
+    
+    // Get Balance factor of node n  
+    int getBalance(Node *n)  
+    {  
+        if (n == nullptr) { return 0; }
+        return height(n->left) - height(n->right);
+    }  
+  
+    // Recursive function to insert a key 
+    // in the subtree rooted with node and 
+    // returns the new root of the subtree.  
+    Node* insert(Node* n, string key)  
+    {  
+        /* 1. Perform the normal BST insertion */
+        if (n == nullptr) { return create_node(key); }
+    
+        if (key < n->word) { n->left = insert(n->left, key); }
+              
+        else if (key > n->word) { n->right = insert(n->right, key); }
+
+        else // Equal keys are not allowed in BST, increment count
+        {
+            n->count++;
+            return n;
+        }
+              
+    
+        /* 2. Update height of this ancestor node */
+        n->height = 1 + max(height(n->left),  
+                            height(n->right));  
+    
+        /* 3. Get the balance factor of this ancestor  
+            node to check whether this node became  
+            unbalanced */
+        int balance = getBalance(n);  
+    
+        // If this node becomes unbalanced, then  
+        // there are 4 cases  
+    
+        // Left Left Case  
+        if (balance > 1 && key < n->left->word)  
+            return rightRotate(n);  
+    
+        // Right Right Case  
+        if (balance < -1 && key > n->right->word)  
+            return leftRotate(n);  
+    
+        // Left Right Case  
+        if (balance > 1 && key > n->left->word)  
+        {  
+            n->left = leftRotate(n->left);  
+            return rightRotate(n);  
+        }  
+    
+        // Right Left Case  
+        if (balance < -1 && key < n->right->word)  
+        {  
+            n->right = rightRotate(n->right);  
+            return leftRotate(n);  
+        }  
+    
+        /* return the (unchanged) node pointer */
+        return n;  
+    }  
+
+    void print_words() const
+    {
+        int count = 1;
+        display(root, count);
+    }
+
+
+    void display(Node *n, int &count) const
+    {
+        if (n != nullptr) 
+        {
+            display(n->left, count);
+            cout << count << ". " << "{\"" << n->word << "\", " << n->count << "}" << endl;
+            count++;
+            display(n->right, count);
+        }
+    }
 
 }; // class Wordlist
